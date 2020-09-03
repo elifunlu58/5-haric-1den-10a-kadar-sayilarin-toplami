98c5fb32056881e6d9f72882b726c6dce902223f
elif <48827713+elifunlu58@users.noreply.github.com>
2020-09-03 20:05:07 +0300

    Add files via upload
    c++ dili ile 5 sayısı hariç 1 den 10 a kadar sayıların toplamını bulan program

---
+++ 5haric 1den 10 a kadar sayıların toplamı.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x,y,top=0;
+	cout<<"y:";
+	cin>>y;
+	
+	
+	for(x=1;y<=10;x++)
+	 {
+	 	if(x==5);
+	 	break;
+	 	top=top+x;
+	 	
+	 	
+	 }
+	
+	cout<<"toplam="<<top;
+	return 0;
+	
+	
+	
+	
+	
+}
